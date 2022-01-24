87187482e0e7b8219712289155ff46297a9e5c32
lucascaravia <88347319+lucascaravia@users.noreply.github.com>
2022-01-23 22:31:04 -0300

    Delete models directory

--- models/serviciosUsuarios.js
+++
@@ -1,6 +0,0 @@
-const bd = require('./../utils/bd');
-const serviciosdb = require('./../utils/serviciosdb')
-
-const getAll = () => serviciosdb.getAll('bdd-final-usuarios');
-
-module.exports = {getAll}
